e23ba0b236e26f24601396e6754c901a15a91922
Atena Razm <atenarazm@Atenas-MacBook-Air.local>
2021-09-27 12:22:29 -0700

    the basic list items are displayed in <li>, Average should be calculated

--- src/components/DisplayCard/index.js
+++ src/components/DisplayCard/index.js
@@ -4,7 +4,19 @@ const DisplayCard = ({ list }) => {
       {" "}
       <ul>
         {list?.map((student) => (
-          <li key={student.id}>{student.firstName}</li>
+          <li key={student.id}>
+            <ul>
+              <li>
+                <img src={`${student.pic}`} />{" "}
+              </li>
+              <li>{`${student.firstName} ${student.lastName}`}</li>
+              <li>{`Email: ${student.email}`}</li>
+              <li>{`Company: ${student.company}`}</li>
+              <li>{`Skill: ${student.skill}`}</li>
+              {/* <li>{`Average: ${student.email}`}</li> */}
+              <hr></hr>
+            </ul>
+          </li>
         ))}
       </ul>
     </div>
