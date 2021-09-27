9e1d0f72ed5b68008e30e5b47e9b97a5839c3243
Atena Razm <atenarazm@Atenas-MacBook-Air.local>
2021-09-27 11:45:07 -0700

    api call is getting the response back

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,4 +1,12 @@
+import { useState, useEffect } from "react";
+import { getData } from "../api-utils";
+
 const Main = () => {
+  const [arr, setArr] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
+
   return <div>hi</div>;
 };
 
