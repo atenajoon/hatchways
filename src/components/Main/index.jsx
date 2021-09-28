5b28d2db6baa8536d3bc13162920ed832eb17b5e
Atena Razm <atenarazm@Atenas-MacBook-Air.local>
2021-09-27 21:17:52 -0700

    value of input is set to the state

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -4,6 +4,7 @@ import DisplayCard from "../DisplayCard";
 
 const Main = () => {
   const [studentList, setStudentList] = useState([]);
+  const [value, setValue] = useState("");
 
   useEffect(() => {
     setList();
@@ -14,9 +15,19 @@ const Main = () => {
     setStudentList(data);
   };
 
+  const handleChange = (e) => {
+    const { value } = e.target;
+    setValue(value);
+  };
   return (
     <div className="main-container">
-      <input id="name-input" placeholder="Search by name" />
+      <input
+        id="name-input"
+        placeholder="Search by name"
+        onChange={handleChange}
+        // value={value}
+        autoComplete="off"
+      />
       <DisplayCard list={studentList.students} />
     </div>
   );
