26b3ab93acbe2592fa5cc2e2a8506038fd736463
Atena Razm <atenarazm@Atenas-MacBook-Air.local>
2021-09-30 09:59:31 -0700

    handleInputChange sets the value to the state, and handlePressEnter is defined

--- src/components/AddTags/index.js
+++ src/components/AddTags/index.js
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 
 const AddTags = ({ student }) => {
+  const [value, setValue] = useState("");
+  const [tag, setTag] = useState([]);
+
   const handleInputChange = (e) => {
     const { value } = e.target;
+    setValue(value);
     console.log(value);
   };
 
+  const handlePressEnter = (e) => {
+    if (e.key === "Enter") {
+      console.log("do validate");
+    }
+  };
   return (
     <div className="tags-container">
-      <div>{`Tag ${student.id}`}</div>
+      <div>{tag}</div>
       <input
         placeholder="Add a tag"
-        // value={value}
+        value={value}
         onChange={handleInputChange}
+        onKeyDown={handlePressEnter}
+        className="​add-tag-input add-input"
       />
     </div>
   );
