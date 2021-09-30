0de3de07622ab6abcd116cc64913c3aeda13cd98
Atena Razm <atenarazm@Atenas-MacBook-Air.local>
2021-09-29 18:39:49 -0700

    button tag toggles between + & - on click

--- src/components/common/Button/index.js
+++ src/components/common/Button/index.js
@@ -1,12 +1,23 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faMinus } from "@fortawesome/free-solid-svg-icons";
+
 const Button = ({ student }) => {
+  const [myIcon, setMyIcon] = useState(faPlus);
+
+  console.log(myIcon);
   const handleClick = (student) => {
     const gradesList = document.getElementById(student.id);
     gradesList.classList.toggle("toggleView");
 
-    console.log("clicked", student.id);
+    myIcon === faPlus ? setMyIcon(faMinus) : setMyIcon(faPlus);
   };
 
-  return <button onClick={() => handleClick(student)}>+</button>;
+  return (
+    <button onClick={() => handleClick(student)}>
+      <FontAwesomeIcon icon={myIcon} />
+    </button>
+  );
 };
 
 export default Button;
