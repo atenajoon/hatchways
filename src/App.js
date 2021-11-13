cfac1c2ecde64da63dccfeff09191f86082fd4b4
atenajoon <atenarazm@gmail.com>
2021-11-12 17:09:59 -0800

    displaying the total number of students from the store in App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
 import Main from "./components/Main";
+import { useSelector, useDispatch } from "react-redux";
 
 function App() {
+  // const dispatch = useDispatch();
+  const students = useSelector((state) => state.studentList);
+
   return (
     <div className="App">
+      <div id="total-result">
+        <span>{students.length} Students</span>
+      </div>
       <Main />
     </div>
   );
