59f8b08ddd38c6298f11ad790c7babc1885d6f34
atenajoon <atenarazm@gmail.com>
2021-11-17 10:48:06 -0800

    filtering logic moved back to the componenr from reducer

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -27,8 +27,40 @@ const SearchBar = ({ id, placeholder, onChange, dataSource }) => {
   };
 
   const handleFilter = (inputId, updatedSearchedTerm, value) => {
-    console.log("passed: ", inputId, updatedSearchedTerm, value);
-    dispatch(filterStudents({ inputId, updatedSearchedTerm, value }));
+    console.log("passed: ", dataSource, inputId, updatedSearchedTerm, value);
+
+    const filteredList = dataSource.filter((student) => {
+      const studentName = `${student.firstName} ${student.lastName}`;
+      const studentTags = student.tags;
+
+      let isNameValid;
+      let isTagValid;
+      const isNameInput = inputId === "name-input";
+
+      isNameValid =
+        updatedSearchedTerm.searchedName.length === 0 // name input field is empty
+          ? true // all names will be valid
+          : studentName // only names that include the term will be valid
+              .toLowerCase()
+              .includes(isNameInput ? value : updatedSearchedTerm.searchedName);
+
+      isTagValid =
+        updatedSearchedTerm.searchedTag.length === 0 // tag input field is empty
+          ? true // all tags will be valid
+          : studentTags?.some(
+              (
+                tag // if tag exist, check if some of them...
+              ) =>
+                tag // only those ones that include the term will be valid
+                  .toLowerCase()
+                  .includes(
+                    !isNameInput ? value : updatedSearchedTerm.searchedTag
+                  )
+            );
+
+      return isNameValid && isTagValid; // return those that have true && true
+    });
+    dispatch(filterStudents({ filteredList }));
 
     // onChange(filteredList);
   };
