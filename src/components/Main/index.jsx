916b9047d7fa9e745c3bf0f7aea3a5ebd95ff6d4
atenajoon <atenarazm@gmail.com>
2021-11-18 12:05:08 -0800

    extra props and code blocks removed in Main component

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import DisplayCards from "../DisplayCards";
 import SearchBar from "../SearchBar";
@@ -6,36 +6,17 @@ import { getStudentsAsync } from "../../redux/studentListSlice";
 
 const Main = () => {
   const dispatch = useDispatch();
-  // set the students based on filter state
   const students = useSelector((state) => state.studentList);
 
   useEffect(() => {
     dispatch(getStudentsAsync());
   }, [dispatch]);
 
-  const handleChange = (filteredList) => {
-    // setStudentList(filteredList);
-  };
-
   return (
     <div className="main-container">
       <div id="id" className="searchBars-container">
-        <SearchBar
-          className="input-box"
-          id="name-input"
-          placeholder="Search by name"
-          onChange={handleChange}
-          // dataSource={mainArray}
-          dataSource={students} // for now!
-        />
-        <SearchBar
-          className="input-box"
-          id="tag-input"
-          placeholder="Search by tags"
-          onChange={handleChange}
-          // dataSource={mainArray}
-          dataSource={students} // for now!
-        />
+        <SearchBar id="name-input" placeholder="Search by name" />
+        <SearchBar id="tag-input" placeholder="Search by tags" />
       </div>
       <div className="cards-container">
         <DisplayCards />
