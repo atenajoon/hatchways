92e258a43fd83e8dee128927ec1e60a6e100b85a
atenajoon <atenarazm@gmail.com>
2021-11-18 11:31:19 -0800

    SearchBar component is only dispatching update reducers to handle the input changes

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,8 +1,7 @@
-// import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { updateName, updateTag } from "../../redux/updateInputSlice";
 
-const SearchBar = ({ id, placeholder, onChange, dataSource }) => {
+const SearchBar = ({ id, placeholder }) => {
   const dispatch = useDispatch();
 
   const searchedTerm = useSelector((state) => state.updateInput);
@@ -14,43 +13,6 @@ const SearchBar = ({ id, placeholder, onChange, dataSource }) => {
     id === "name-input"
       ? dispatch(updateName({ tag: lowerCaseValue }))
       : dispatch(updateTag({ tag: lowerCaseValue }));
-
-    //   // dispatch(filteredList({ }))
-    // handleFilter(id, lowerCaseValue);
-  };
-
-  const handleFilter = (inputId, updatedSearchedTerm, value) => {
-    const filteredList = dataSource.filter((student) => {
-      const studentName = `${student.firstName} ${student.lastName}`;
-      const studentTags = student.tags;
-
-      let isNameValid;
-      let isTagValid;
-      const isNameInput = inputId === "name-input";
-
-      isNameValid =
-        updatedSearchedTerm.searchedName.length === 0
-          ? true
-          : studentName
-              .toLowerCase()
-              .includes(isNameInput ? value : updatedSearchedTerm.searchedName);
-
-      isTagValid =
-        updatedSearchedTerm.searchedTag.length === 0
-          ? true
-          : studentTags?.some((tag) =>
-              tag
-                .toLowerCase()
-                .includes(
-                  !isNameInput ? value : updatedSearchedTerm.searchedTag
-                )
-            );
-
-      return isNameValid && isTagValid;
-    });
-
-    console.log("pass this to the App.js!", filteredList.length);
-    onChange(filteredList);
   };
 
   return (
