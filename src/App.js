419d650edde37ed0834a6a2feb1755ed94372983
atenajoon <atenarazm@gmail.com>
2021-11-23 10:38:19 -0800

    total comments removed from App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import "./App.css";
 import Main from "./components/Main";
-// import { useSelector } from "react-redux";
-
 function App() {
-  // const students = useSelector((state) => state.studentList);
-
   return (
     <div className="App">
-      {/* <div id="total-result">
-        <span>{students.length} Students</span>
-      </div> */}
       <Main />
     </div>
   );
