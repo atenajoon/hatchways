17c0f3caf150e1d151180fa5bd7e1c5e36a94f06
atenajoon <atenarazm@gmail.com>
2021-11-23 15:54:46 -0800

    testing file name changes

--- src/components/DisplayCards/utils.js
+++ src/components/DisplayCards/utils.js
@@ -1,4 +1,4 @@
-import { getFullName } from "../../utils/business-utils";
+import { getFullName } from "../../myU/business-utils";
 
 export const getStudentList = (studentListState, searchedName, searchedTag) => {
   if (searchedName === "" && searchedTag === "") {
