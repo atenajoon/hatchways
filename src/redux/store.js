aa155cb471eaa7e9b06ce7e559835c1f36adb535
atenajoon <atenarazm@gmail.com>
2021-11-22 19:30:18 -0800

    adding the totalStudentsSlice to the store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import updateInputReducer from "./updateInputSlice";
 import studentListReducer from "./studentListSlice";
+import totalStudentsReducer from "./totalStudentsSlice";
 
 export default configureStore({
   reducer: {
     studentList: studentListReducer,
     updateInput: updateInputReducer,
+    totalStudents: totalStudentsReducer,
   },
 });
