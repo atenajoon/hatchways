17cf70a6b580fcbc706012ff1963134027ab37b5
atenajoon <atenarazm@gmail.com>
2021-11-22 19:32:08 -0800

    totalStudents action is patched in DisplayCards

--- src/components/DisplayCards/index.js
+++ src/components/DisplayCards/index.js
@@ -1,18 +1,21 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import StudentCard from "../StudentCard";
 import { getStudentList } from "./utils";
+import { displayTotalStudents } from "../../redux/totalStudentsSlice";
 
 const DisplayCards = () => {
   const studentListState = useSelector((state) => state.studentList);
   const { searchedName, searchedTag } = useSelector(
     (state) => state.updateInput
   );
+  const dispatch = useDispatch();
   // read about react.memo and use it to prevent rerendering for shallow changes!
   const filteredList = getStudentList(
     studentListState,
     searchedName,
     searchedTag
   );
+  dispatch(displayTotalStudents({ total: filteredList.length }));
 
   return (
     <div>
